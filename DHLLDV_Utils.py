0912d1fb99251be1bc032eca57b1cad2ade2066b
rcriii42 <rcramsdell@gldd.com>
2014-10-31 10:49:52 -0500

    Added interpDict class and apply to lookup tables in DHLLDV_Constants

---
+++ DHLLDV_Utils.py
@@ -0,0 +1,39 @@
+'''
+DHLLDV_Utils: Utility functions.
+
+Created on Oct 23, 2014
+
+@author: RCRamsdell
+'''
+import bisect
+
+class interpDict(dict):
+    """
+    interpDict: Dict of two-tuples that will interpolate values not found in the dict.
+    """
+    def __init__(self, *args):
+        #print type(args[0]), args
+        if type(args[0])==type(dict()):
+            self.update(args[0])
+        else:
+            dict.__init__(self, args)
+
+    def __getitem__(self, key):
+        try:
+            val = dict.__getitem__(self, key)
+        except KeyError:
+            keys = self.keys()
+            keys.sort()
+            index = bisect.bisect(keys, key)
+            if index and index!=len(keys):
+                x1 = keys[index-1]
+                x2 = keys[index]
+                y1 = dict.__getitem__(self, x1)
+                y2 = dict.__getitem__(self, x2)
+                val = ((y2-y1)/(x2-x1))*(key-x1)+y1
+            else:
+                raise IndexError("key out of range")
+        return val
+
+    def __setitem__(self, key, val):
+        raise KeyError("interpDict is read-only")
---
+++ test_utils.py
@@ -0,0 +1,41 @@
+'''
+Created on Oct 23, 2014
+
+@author: RCRamsdell
+'''
+import unittest
+import DHLLDV_Utils
+
+
+class TestInterpDict(unittest.TestCase):
+
+    def testInstantiateWithDict(self):
+        t1 = DHLLDV_Utils.interpDict({00:0.99984, #deg C, density in ton/m**3 per wikipedia
+                                      04:0.99997,
+                                      05:0.99996,
+                                      10:0.99970,
+                                      15:0.99910,
+                                      20:0.99820,})
+        self.assertAlmostEqual(t1[12.5],(0.9997+0.9991)/2)
+        
+    def testInterpolation(self):
+        t1 = DHLLDV_Utils.interpDict((1,20), (2,30), (3,50))
+        self.assertEqual(t1[2.5],40) 
+        
+    def testBelowRange(self):
+        t1 = DHLLDV_Utils.interpDict((1,20), (2,30), (3,50))
+        self.assertRaises(IndexError, t1.__getitem__,19)
+        
+    def testAboveRange(self):
+        t1 = DHLLDV_Utils.interpDict((1,20), (2,30), (3,50))
+        self.assertRaises(IndexError, t1.__getitem__,60)
+        
+    def testReadOnly(self):
+        t1 = DHLLDV_Utils.interpDict((1,20), (2,30), (3,50))
+        self.assertRaises(KeyError, t1.__setitem__, 4, 75)
+        
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
