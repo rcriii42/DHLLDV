cbc636a5b40a297d991956c5910015fb36fff7a1
Robert Ramsdell <rcramsdell@gldd.com>
2015-02-21 16:29:45 -0600

    Fixed bed pressure loss and tests

--- stratified.py
+++ stratified.py
@@ -9,7 +9,6 @@ Created on Oct 22, 2014
 from DHLLDV_constants import gravity, Arel_to_beta
 from math import pi, sin, log
 
-
 def beta(Cvs, Cvb=0.6):
     """Return the angle beta based on the Cvs and Cvb"""
     return Arel_to_beta[Cvs/Cvb]
@@ -26,7 +25,6 @@ def areas(Dp, Cvs, Cvb=0.6):
     A1 = Ap - A2
     return Ap, A1, A2
 
-
 def perimeters(Dp, Cvs, Cvb=0.6):
     """Return the four perimeters:
        Op  = The perimeter of the pipe
@@ -41,7 +39,6 @@ def perimeters(Dp, Cvs, Cvb=0.6):
     O2 = Op - O1
     return Op, O1, O12, O2
 
-
 def lambda1(Dp_H, v1, epsilon, nu_l):
     """Return the friction factor for the pipewall above the bed (eqn 8.3-12)
        Dp_H = Hydraulic radius of the pipe above the bed (m)
@@ -55,15 +52,13 @@ def lambda1(Dp_H, v1, epsilon, nu_l):
     bottom = log(c1+c2)**2
     return 1.325/bottom
 
-
-def lambda12(Dp_H, d, v1, v2, rho_l, nu_l):
+def lambda12(Dp_H, d, v1, v2, nu_l):
     """Return the bed friction factor lambda12 (eqn 8.3-13, no sheet flow)
-       Dp = Pipe diameter (m)
+       Dp_H = Hydraulic radius of the pipe above the bed (m)
        d = Particle diameter (m)
        v1 = velocity of the fluid above the bed (m/sec)
        v2 = velocity of the bed (m/sec)
        nu_l = fluid kinematic viscosity in m2/sec
-       rho_l = density of the fluid (ton/m3)
     """
     Re = (v1 - v2) * Dp_H/nu_l
     c1 = 0.27 * d/Dp_H
@@ -71,21 +66,60 @@ def lambda12(Dp_H, d, v1, v2, rho_l, nu_l):
     bottom = log(c1+c2)**2
     return 1.325/bottom
 
-
-def lambda12_sf(Dp_H, d, v1, v2, epsilon, rho_s, rho_l, nu_l):
+def lambda12_sf(Dp_H, d, v1, v2, epsilon, nu_l, rho_l, rho_s):
     """Return the bed friction factor lambda12 (eqn 8.3-14, with sheet flow)
        Dp = Pipe diameter (m)
        d = Particle diameter (m)
        v1 = velocity of the fluid above the bed (m/sec)
        v2 = velocity of the bed (m/sec)
+       epsilon = absolute pipe roughness (m)
        nu_l = fluid kinematic viscosity in m2/sec
        rho_l = density of the fluid (ton/m3)
+       rho_s = particle density (ton/m3)
     """
     Rsd = (rho_s - rho_l)/rho_l
     first = ((v1-v2)/(2*gravity*Dp_H*Rsd)**0.5)**2.73
     second = ((rho_s*(pi/6)*d**3)/rho_l)**0.094
     return 0.83*lambda1(Dp_H, v1, epsilon, nu_l) + 0.37*first*second
 
+def fb_pressure_loss(vls, Dp,  d, epsilon, nu, rhol, rhos, Cvs, Cvb=0.6):
+    """Return the pressure loss for fluid above a fixed bed.
+       vls = average line speed (velocity, m/sec) 
+       Dp = Pipe diameter (m)
+       d = Particle diameter (m)
+       epsilon = absolute pipe roughness (m)
+       nu = fluid kinematic viscosity in m2/sec
+       rhol = density of the fluid (ton/m3)
+       rhos = particle density (ton/m3)
+       Cvs = insitu volume concentration
+    """
+    Ap, A1, A2 = areas(Dp, Cvs, Cvb)
+    Op, O1, O12, O2 = perimeters(Dp, Cvs, Cvb)
+    Dp_H = 4*A1/(O1 + O12)
+    v1 = vls*Ap/A1
+    v2 = 0.0
+    lbd1 = lambda1(Dp_H, v1, epsilon, nu)
+    tau1_l = lbd1*rhol*v1**2/8
+    F1_l = tau1_l * O1
+    lbd12 = min(lambda12(Dp_H, d, v1, v2, nu),
+                lambda12_sf(Dp_H, d, v1, v2, epsilon, nu, rhol, rhos))
+    tau12_l = lbd12*rhol*v1**2/8
+    F12_l = tau12_l * O12
+    return (F1_l + F12_l)/A1
+
+def fb_head_loss(vls, Dp,  d, epsilon, nu, rhol, rhos, Cvs, Cvb=0.6):
+    """Return the head loss for fluid above a fixed bed.
+       vls = average line speed (velocity, m/sec) 
+       Dp = Pipe diameter (m)
+       d = Particle diameter (m)
+       epsilon = absolute pipe roughness (m)
+       nu = fluid kinematic viscosity in m2/sec
+       rhol = density of the fluid (ton/m3)
+       rhos = particle density (ton/m3)
+       Cvs = insitu volume concentration
+    """
+    delta_p = fb_pressure_loss(vls, Dp,  d, epsilon, nu, rhol, rhos, Cvs, Cvb)
+    return delta_p * rhol/gravity
 
 if __name__ == '__main__':
     pass
--- test_stratified.py
+++ test_stratified.py
@@ -22,7 +22,7 @@ class Test(unittest.TestCase):
     def test_perimeters(self):
         Op, O1, O12, O2 = stratified.perimeters(0.5, 0.1)
         self.assertAlmostEqual(Op, 1.5707963)
-        ##Reduced precision because Beta only to 4 places
+        #Reduced precision because Beta only to 4 places
         self.assertAlmostEqual(O1, 1.0788463, places=4)
         self.assertAlmostEqual(O12, 0.4163317, places=4)
         self.assertAlmostEqual(O2, 0.4919500, places=4)
@@ -34,7 +34,47 @@ class Test(unittest.TestCase):
         v1 = 2*Ap/A1
         epsilon = DHLLDV_constants.steel_roughness
         nu_l = DHLLDV_constants.water_viscosity[20]
-        self.assertAlmostEqual(stratified.lambda1(Dp_H, v1, epsilon, nu_l), 1.336871e-02, places=3)
+        self.assertAlmostEqual(stratified.lambda1(Dp_H, v1, epsilon, nu_l), 
+                               0.01349789)
+    
+    def test_lambda12(self):
+        Ap, A1, A2 = stratified.areas(0.5, 0.1)
+        Op, O1, O12, O2 = stratified.perimeters(0.5, 0.1)
+        Dp_H = 4*A1/(O1+O12)
+        v1 = 2*Ap/A1
+        v2 = 0.0
+        d = 0.3
+        nu_l = DHLLDV_constants.water_viscosity[20]
+        self.assertAlmostEqual(stratified.lambda12(Dp_H, d, v1, v2, nu_l), 
+                               0.46554005, places=5)
+    
+    def test_lambda12_sf(self):
+        Ap, A1, A2 = stratified.areas(0.5, 0.1)
+        Op, O1, O12, O2 = stratified.perimeters(0.5, 0.1)
+        Dp_H = 4*A1/(O1+O12)
+        d=0.3
+        v1 = 2*Ap/A1
+        v2 = 0.0
+        epsilon = DHLLDV_constants.steel_roughness
+        nu_l = DHLLDV_constants.water_viscosity[20]
+        rho_s = 2.65
+        rho_l = DHLLDV_constants.water_density[20]
+        self.assertAlmostEqual(stratified.lambda12_sf(Dp_H, d, v1, v2, epsilon, nu_l, rho_l, rho_s),
+                               0.09045342, places=6)
+    
+    def test_fb_pressure_loss(self):
+        vls = 2.0
+        Dp = 0.5
+        d = 0.3
+        epsilon = DHLLDV_constants.steel_roughness
+        nu_l = DHLLDV_constants.water_viscosity[20]
+        rho_s = 2.65
+        rho_l = DHLLDV_constants.water_density[20]
+        Cvs = 0.1
+        self.assertAlmostEqual(stratified.fb_pressure_loss(vls, Dp, d, epsilon, nu_l, rho_l, rho_s, Cvs),
+                               0.22937437, places=5)
+        self.assertAlmostEqual(stratified.fb_head_loss(vls, Dp, d, epsilon, nu_l, rho_l, rho_s, Cvs),
+                               0.02334757, places=6)
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
