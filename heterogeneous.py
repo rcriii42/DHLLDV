ded236bbec35d7946ab88d610717b370750c285c
Robert Ramsdell <rcramsdell@gldd.com>
2015-03-01 08:15:26 -0600

    Updated heterogeneous and tests for sliding flow. Added particle_ratio and musf as constants.

--- heterogeneous.py
+++ heterogeneous.py
@@ -8,6 +8,9 @@ from DHLLDV_constants import gravity, Arel_to_beta
 import homogeneous
 from math import pi, sin, log, cosh
 
+particle_ratio = 0.015          #Particle to pipe diameter ratio for sliding flow per Sellgren & Wilson
+from stratified import musf     #Sliding friction factor
+
 def vt_grace(d, Rsd, nu, K=0.26):
     """vt_grace - terminal settling velocity via the Grace method (equations 4.4-21 - 4.4-27)
        d particle diameter (m)
@@ -28,7 +31,7 @@ def vt_grace(d, Rsd, nu, K=0.26):
     xi = 10**(-0.55+K-0.0015*K**(-2)+0.03*(1000)**(K-0.524)+(-0.045+0.05*K**(-0.6)-0.0287*(55000)**(K-0.524))/cosh(2.55*(log(Dstar,10)-1.114)))
     return xi*vts
 
-def Erhg(vls, Dp,  d, epsilon, nu, rhol, rhos, Cvs):
+def Erhg(vls, Dp,  d, epsilon, nu, rhol, rhos, Cvs, use_sf = True):
     """Relative excess pressure gradient, per equation 8.5-2
        vls = average line speed (velocity, m/sec)
        Dp = Pipe diameter (m)
@@ -38,6 +41,7 @@ def Erhg(vls, Dp,  d, epsilon, nu, rhol, rhos, Cvs):
        rhol = density of the fluid (ton/m3)
        rhos = particle density (ton/m3)
        Cvs = insitu volume concentration
+       use_sf: Whether to apply the sliding flow correction
     """
     Rsd = (rhos-rhol)/rhol
     vt = vt_grace(d, Rsd, nu)
@@ -51,10 +55,16 @@ def Erhg(vls, Dp,  d, epsilon, nu, rhol, rhos, Cvs):
     Re = homogeneous.pipe_reynolds_number(vls, Dp, nu)
     lbdl = homogeneous.swamee_jain_ff(Re, Dp, epsilon)
     Srs = 7.5**2 * (1/lbdl) * (vt/(gravity*d)**0.5)**(8./3.) * ((nu*gravity)**(1./3.)/vls)**2  #Eqn 8.5-2
-    
-    return Shr + Srs
+    f = d/(particle_ratio * Dp)  #eqn 8.7-4
+    if not use_sf or f<1:
+        return Shr + Srs
+    else:
+        #Sliding flow per equation 8.7-4 and 8.7-5
+          
+        return (Shr + Srs + (f-1)*musf)/f
+        
 
-def heterogeneous_pressure_loss(vls, Dp,  d, epsilon, nu, rhol, rhos, Cvs):
+def heterogeneous_pressure_loss(vls, Dp,  d, epsilon, nu, rhol, rhos, Cvs, use_sf = True):
     """Return the pressure loss (delta_pm in kPa per m) for heterogeneous flow.
        vls = average line speed (velocity, m/sec)
        Dp = Pipe diameter (m)
@@ -65,9 +75,9 @@ def heterogeneous_pressure_loss(vls, Dp,  d, epsilon, nu, rhol, rhos, Cvs):
        rhos = particle density (ton/m3)
        Cvs = insitu volume concentration
     """
-    return heterogeneous_head_loss(vls, Dp,  d, epsilon, nu, rhol, rhos, Cvs)*gravity/rhol
+    return heterogeneous_head_loss(vls, Dp,  d, epsilon, nu, rhol, rhos, Cvs, use_sf)*gravity/rhol
 
-def heterogeneous_head_loss(vls, Dp,  d, epsilon, nu, rhol, rhos, Cvs):
+def heterogeneous_head_loss(vls, Dp,  d, epsilon, nu, rhol, rhos, Cvs, use_sf = True):
     """Return the head loss (m.w.c per m) for (pseudo) heterogeneous flow.
        vls = average line speed (velocity, m/sec)
        Dp = Pipe diameter (m)
@@ -80,7 +90,7 @@ def heterogeneous_head_loss(vls, Dp,  d, epsilon, nu, rhol, rhos, Cvs):
     """
     il = homogeneous.fluid_head_loss(vls, Dp, epsilon, nu, rhol)
     Rsd = (rhos-rhol)/rhol
-    return Erhg(vls, Dp,  d, epsilon, nu, rhol, rhos, Cvs)*Rsd*Cvs + il
+    return Erhg(vls, Dp,  d, epsilon, nu, rhol, rhos, Cvs, use_sf)*Rsd*Cvs + il
 
 if __name__ == '__main__':
     pass
--- test_heterogeneous.py
+++ test_heterogeneous.py
@@ -22,7 +22,7 @@ class Test(unittest.TestCase):
         self.assertAlmostEqual(heterogeneous.vt_grace(10./1000, Rsd, nu), 0.3642040, places=2)
         self.assertAlmostEqual(heterogeneous.vt_grace(75./1000, Rsd, nu), 0.9010960, places=1)
         
-    def test_Ehrg1(self):
+    def test_Ehrg_nosf(self):
         vls = 3.0
         Dp = 0.5
         epsilon = DHLLDV_constants.steel_roughness
@@ -30,12 +30,27 @@ class Test(unittest.TestCase):
         rhos = 2.65
         rhol = DHLLDV_constants.water_density[20]
         Cvs = 0.1
-        self.assertAlmostEqual(heterogeneous.Erhg(vls, Dp, 0.0075/1000, epsilon, nu, rhol, rhos, Cvs), 0.0000051)
-        self.assertAlmostEqual(heterogeneous.Erhg(vls, Dp, 0.2/1000, epsilon, nu, rhol, rhos, Cvs), 0.0119203)
-        self.assertAlmostEqual(heterogeneous.Erhg(vls, Dp, 0.4/1000, epsilon, nu, rhol, rhos, Cvs), 0.0528735)
-        self.assertAlmostEqual(heterogeneous.Erhg(vls, Dp, 0.8/1000, epsilon, nu, rhol, rhos, Cvs), 0.1512408)
-        self.assertAlmostEqual(heterogeneous.Erhg(vls, Dp, 1.6/1000, epsilon, nu, rhol, rhos, Cvs), 0.2493150)
-        self.assertAlmostEqual(heterogeneous.Erhg(vls, Dp, 10./1000, epsilon, nu, rhol, rhos, Cvs), 0.4106565, places=1)
+        self.assertAlmostEqual(heterogeneous.Erhg(vls, Dp, 0.0075/1000, epsilon, nu, rhol, rhos, Cvs, use_sf=False), 0.0000051)
+        self.assertAlmostEqual(heterogeneous.Erhg(vls, Dp, 0.2/1000, epsilon, nu, rhol, rhos, Cvs, use_sf=False), 0.0119203)
+        self.assertAlmostEqual(heterogeneous.Erhg(vls, Dp, 0.4/1000, epsilon, nu, rhol, rhos, Cvs, use_sf=False), 0.0528735)
+        self.assertAlmostEqual(heterogeneous.Erhg(vls, Dp, 0.8/1000, epsilon, nu, rhol, rhos, Cvs, use_sf=False), 0.1512408)
+        self.assertAlmostEqual(heterogeneous.Erhg(vls, Dp, 1.6/1000, epsilon, nu, rhol, rhos, Cvs, use_sf=False), 0.2493150)
+        self.assertAlmostEqual(heterogeneous.Erhg(vls, Dp, 10./1000, epsilon, nu, rhol, rhos, Cvs, use_sf=False), 0.4106565, places=1)
+        
+    def test_Ehrg_with_sf(self):
+        vls = 3.0
+        Dp = 0.5
+        epsilon = DHLLDV_constants.steel_roughness
+        nu = 0.001005/(0.9982*1000)
+        rhos = 2.65
+        rhol = DHLLDV_constants.water_density[20]
+        Cvs = 0.1
+        self.assertAlmostEqual(heterogeneous.Erhg(vls, Dp, 0.0075/1000, epsilon, nu, rhol, rhos, Cvs, use_sf=True), 0.0000051)
+        self.assertAlmostEqual(heterogeneous.Erhg(vls, Dp, 0.2/1000, epsilon, nu, rhol, rhos, Cvs, use_sf=True), 0.0119203)
+        self.assertAlmostEqual(heterogeneous.Erhg(vls, Dp, 0.4/1000, epsilon, nu, rhol, rhos, Cvs, use_sf=True), 0.0528735)
+        self.assertAlmostEqual(heterogeneous.Erhg(vls, Dp, 0.8/1000, epsilon, nu, rhol, rhos, Cvs, use_sf=True), 0.1512408)
+        self.assertAlmostEqual(heterogeneous.Erhg(vls, Dp, 1.6/1000, epsilon, nu, rhol, rhos, Cvs, use_sf=True), 0.2493150)
+        self.assertAlmostEqual(heterogeneous.Erhg(vls, Dp, 10./1000, epsilon, nu, rhol, rhos, Cvs, use_sf=True), 0.4117424, places=1)
         
     def test_head_loss(self):
         vls = 3.0
@@ -46,8 +61,8 @@ class Test(unittest.TestCase):
         rhos = 2.65
         rhol = DHLLDV_constants.water_density[20]
         Cvs = 0.1
-        self.assertAlmostEqual(heterogeneous.heterogeneous_head_loss(vls, Dp, d, epsilon, nu, rhol, rhos, Cvs), 0.0206256)
-        self.assertAlmostEqual(heterogeneous.heterogeneous_pressure_loss(vls, Dp, d, epsilon, nu, rhol, rhos, Cvs), 0.2026330)
+        self.assertAlmostEqual(heterogeneous.heterogeneous_head_loss(vls, Dp, d, epsilon, nu, rhol, rhos, Cvs, use_sf=False), 0.0206256)
+        self.assertAlmostEqual(heterogeneous.heterogeneous_pressure_loss(vls, Dp, d, epsilon, nu, rhol, rhos, Cvs, use_sf=False), 0.2026330)
 
 
 if __name__ == "__main__":
