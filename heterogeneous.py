0626486fb8bf80398ffc2226268b73496d122e20
Robert Ramsdell <rcramsdell@gldd.com>
2016-06-21 16:53:56 -0500

    Updated eqn numbers in heterogeneous.py to 1st ed of book.

--- heterogeneous.py
+++ heterogeneous.py
@@ -18,7 +18,8 @@ def vt_ruby(d, Rsd, nu, K=0.26):
     """
     right = 10*nu/d
     left = (1+(Rsd*gravity*d**3)/(100*nu**2))**0.5 -1
-    return right * left
+    return right * left #Eqn 8.2-2
+
 
 def Erhg(vls, Dp,  d, epsilon, nu, rhol, rhos, Cvs, use_sf = True):
     """Relative excess pressure gradient, per equation 8.5-2
@@ -34,16 +35,16 @@ def Erhg(vls, Dp,  d, epsilon, nu, rhol, rhos, Cvs, use_sf = True):
     """
     Rsd = (rhos-rhol)/rhol
     vt = vt_ruby(d, Rsd, nu)
-    Rep = vt*d/nu  #eqn 4.2-6
+    Rep = vt*d/nu  #eqn 8.2-4
     top = 4.7 + 0.41*Rep**0.75
     bottom = 1. + 0.175*Rep**0.75
-    beta = top/bottom  #eqn 4.6-4
+    beta = top/bottom  #eqn 8.2-4
     KC = 0.175*(1+beta)
-    Shr = vt*(1-Cvs/KC)**beta /vls #Eqn 8.6-2
+    Shr = vt*(1-Cvs/KC)**beta /vls #Eqn 8.5-2
     
     Re = homogeneous.pipe_reynolds_number(vls, Dp, nu)
     lbdl = homogeneous.swamee_jain_ff(Re, Dp, epsilon)
-    Srs = 8.5**2 * (1/lbdl) * (vt/(gravity*d)**0.5)**(10./3.) * ((nu*gravity)**(1./3.)/vls)**2  #Eqn 8.6-2
+    Srs = 8.5**2 * (1/lbdl) * (vt/(gravity*d)**0.5)**(10./3.) * ((nu*gravity)**(1./3.)/vls)**2  #Eqn 8.5-2
     f = d/(particle_ratio * Dp)  #eqn 8.8-4
     if not use_sf or f<1:
         return Shr + Srs
