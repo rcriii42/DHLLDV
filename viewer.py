123e3252106a59858a1ff865de602d6157486369
rcriii42 <rcriii@ramsdells.net>
2021-02-02 22:18:24 -0600

    Update graph to match parameters in Figure 15.5-1/15.5-2 (3rd ed)
    Not quite as that figure uses salt water

--- viewer.py
+++ viewer.py
@@ -21,18 +21,18 @@ except:
 
 if __name__ == '__main__':
     Dp = 0.762  #Pipe diameter
-    d = 2/1000.
+    d = 1/1000.
     epsilon = DHLLDV_constants.steel_roughness
     nu = DHLLDV_constants.water_viscosity[20] 
     rhos = 2.65
     rhol = DHLLDV_constants.water_density[20]
     Rsd = (rhos - rhol)/rhol
-    Cvs = 0.25
-    rhom = Cvs*(rhos-rhol)+rhol
+    Cv = 0.175
+    rhom = Cv*(rhos-rhol)+rhol
     
     vls_list = [(i+1)/10. for i in range(200)]
     il_list = [homogeneous.fluid_head_loss(vls, Dp, epsilon, nu, rhol) for vls in vls_list]
-    Erhg_obj_list = [DHLLDV_framework.Cvs_Erhg(vls, Dp, d, epsilon, nu, rhol, rhos, Cvs, get_dict=True) for vls in vls_list]
+    Erhg_obj_list = [DHLLDV_framework.Cvs_Erhg(vls, Dp, d, epsilon, nu, rhol, rhos, Cv, get_dict=True) for vls in vls_list]
     
     #The Erhg Curves
     Erhg_list = [Erhg_obj[Erhg_obj['regime']] for Erhg_obj in Erhg_obj_list]
@@ -41,17 +41,18 @@ if __name__ == '__main__':
     He_Erhg_list = [Erhg_obj['He'] for Erhg_obj in Erhg_obj_list]
     #Erhg for the ELM is just the il
     Ho_Erhg_list = [Erhg_obj['Ho'] for Erhg_obj in Erhg_obj_list]
-    Cvt = Cvs
-    Cvt_Erhg_list = [DHLLDV_framework.Cvt_Erhg(vls, Dp, d, epsilon, nu, rhol, rhos, Cvt) for vls in vls_list]
+    Cvt = Cv
+    Cvs_from_Cvt_list = [DHLLDV_framework.Cvs_from_Cvt(vls, Dp, d, epsilon, nu, rhol, rhos, Cvt) for vls in vls_list]
+    Cvt_Erhg_list = [DHLLDV_framework.Cvt_Erhg(vls, Dp, d, epsilon, nu, rhol, rhos, Cv) for vls in vls_list]
     
     #The im curves
-    im_list = [Erhg_list[i]*Rsd*Cvs+il_list[i] for i in range(200)]
-    FB_im_list = [FB_Erhg_list[i]*Rsd*Cvs+il_list[i] for i in range(200)]
-    SB_im_list = [SB_Erhg_list[i]*Rsd*Cvs+il_list[i] for i in range(200)]
-    He_im_list = [He_Erhg_list[i]*Rsd*Cvs+il_list[i] for i in range(200)]
+    im_list = [Erhg_list[i]*Rsd*Cv+il_list[i] for i in range(200)]
+    FB_im_list = [FB_Erhg_list[i]*Rsd*Cv+il_list[i] for i in range(200)]
+    SB_im_list = [SB_Erhg_list[i]*Rsd*Cv+il_list[i] for i in range(200)]
+    He_im_list = [He_Erhg_list[i]*Rsd*Cv+il_list[i] for i in range(200)]
     ELM_im_list = [il_list[i]*rhom for i in range(200)]
-    Ho_im_list = [Ho_Erhg_list[i]*Rsd*Cvs+il_list[i] for i in range(200)]
-    Cvt_im_list = [Cvt_Erhg_list[i]*Rsd*Cvt+il_list[i] for i in range(200)]
+    Ho_im_list = [Ho_Erhg_list[i]*Rsd*Cv+il_list[i] for i in range(200)]
+    Cvt_im_list = [Cvt_Erhg_list[i]*Rsd*Cv+il_list[i] for i in range(200)]
     
     #The LDV
     Cv_list = [(i+1)/100. for i in range(50)]
@@ -67,8 +68,8 @@ if __name__ == '__main__':
     if plt:
         fig = plt.figure()
         # log x and y axis
-        Erhg_title = "Erhg for Dp=%0.3fm, d=%0.1fmm, Rsd=%0.3f, Cv=%0.3f, rhom=%0.3f"%(Dp, d*1000, Rsd, Cvs, rhom)
-        Erhg_plot = fig.add_subplot(211, title=Erhg_title, xlim=(.001, 1.0), ylim=(0.001, 3))
+        Erhg_title = "Erhg for Dp=%0.3fm, d=%0.1fmm, Rsd=%0.3f, Cv=%0.3f, rhom=%0.3f"%(Dp, d*1000, Rsd, Cv, rhom)
+        Erhg_plot = fig.add_subplot(311, title=Erhg_title, xlim=(.001, 1.0), ylim=(0.001, 3))
         Erhg_plot.loglog(il_list, FB_Erhg_list, linewidth=1, linestyle='--', color='c', label="FB Cvs=c")
         Erhg_plot.loglog(il_list, SB_Erhg_list, linewidth=1, linestyle='--', color='brown', label="SB Cvs=c")
         Erhg_plot.loglog(il_list, He_Erhg_list, linewidth=1, linestyle='--', color='b', label="He Cvs=c")
@@ -82,11 +83,11 @@ if __name__ == '__main__':
             label.set_fontsize('small')
 
 
-        HG_title = "Hydraulic gradient for Dp=%0.3fm, d=%0.1fmm, Rsd=%0.3f, Cv=%0.3f, rhom=%0.3f"%(Dp, d*1000, Rsd, Cvs, rhom)
+        HG_title = "Hydraulic gradient for Dp=%0.3fm, d=%0.1fmm, Rsd=%0.3f, Cv=%0.3f, rhom=%0.3f"%(Dp, d*1000, Rsd, Cv, rhom)
 
         spot10 = vls_list.index(10)
         hg_ymax = max(SB_im_list[spot10], Ho_im_list[spot10], He_im_list[spot10])*1.5
-        HG_plot = fig.add_subplot(212, title=HG_title, xlim=(0,10), ylim=(0,hg_ymax))
+        HG_plot = fig.add_subplot(312, title=HG_title, xlim=(0,10), ylim=(0,hg_ymax))
         HG_plot.plot(vls_list, FB_im_list, linewidth=1, linestyle='--', color='c')
         HG_plot.plot(vls_list, SB_im_list, linewidth=1, linestyle='--', color='brown')
         HG_plot.plot(vls_list, He_im_list, linewidth=1, linestyle='--', color='b')
