ed7e99b91bd03db8a1ac3dde5b5def27678414ef
rcriii42 <rcriii@ramsdells.net>
2021-05-23 12:19:53 -0500

    Vsm

--- Wilson/Wilson_Stratified.py
+++ Wilson/Wilson_Stratified.py
@@ -2,6 +2,10 @@
 Wilson Stratified - Implementing the Wilson stratified model as presented in section 6.20.1
 """
 
+from math import log
+
+from DHLLDV.DHLLDV_constants import Cvb
+
 def Vsm_max(Dp, d, rhol, rhos, musf):
     """Return the maximum velocity at the limit of stationary deposition
 
@@ -26,3 +30,25 @@ def Cvr_max(Dp, d, rhol, rhos):
     """
     Rsd = (rhos - rhol) / rhol  # Eqn 8.2-1
     return 0.16*Dp**0.4 / (d**0.84 * (Rsd/1.65)**0.17)  # Eqn. 6.20-35
+
+def Vsm(Dp, d, rhol, rhos, musf, Cv):
+    """Return the maximum velocity at the limit of stationary deposition
+
+        Dp = Pipe diameter (m)
+        d = Particle diameter (m)
+        rhol = density of the fluid (ton/m3)
+        rhos = particle density (ton/m3)
+        musf = The oefficient of sliding friction
+        Cv = The volume concentration
+    """
+    Cvrmx = Cvr_max(Dp, d, rhol, rhos)
+    Vsmx = Vsm_max(Dp, d, rhol, rhos, musf)
+    Cvr = Cv/Cvb
+    if Cvrmx <= 0.33:
+        alpha = log(0.333)/log(Cvrmx)
+        Vs = Vsmx * 6.75 * Cvr**alpha * (1 - Cvr**alpha)**2 # Eqn. 6.20-36
+    else:
+        beta = log(0.666)/log(1-Cvrmx)
+        Vs = Vsmx * 6.75 * (1-Cvr)**2*beta * (1-(1-Cvr)**beta)  # Eqn. 6.20-36
+
+    return min(Vs, Vsmx)
