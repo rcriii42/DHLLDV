c112739e4ad235eda50469bf57c3b73c33f116f7
rcriii42 <rcriii42@gmail.com>
2021-06-01 11:07:22 -0500

    File for tests of the V50 model

---
+++ src/Wilson/test_Wilson_V50.py
@@ -0,0 +1,15 @@
+"""Tests of the Wilson Stratified flow regime model,
+based on the example and Case Studies in
+Slurry Transport Using Centrifugal Pumps, 2nd ed.,
+by Wilson, Addie, Sellgren and Clift (WASC2)"""
+
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
