066fe4013bb4eeecc131a4e0f8e9a0660ef1cff7
rcriii42 <rcriii42@gmail.com>
2021-09-02 15:30:08 -0500

    Create a pipeline object to manage the system

--- Scripts/SystemTab.py
+++ Scripts/SystemTab.py
@@ -6,6 +6,7 @@ Execute by running 'bokeh serve --show .\Scripts\bokeh_viewer.py' to open a tab 
 Added by R. Ramsdell 01 September, 2021
 """
 import collections
+from copy import copy
 
 from bokeh.io import curdoc
 from bokeh.layouts import column, row
@@ -17,12 +18,37 @@ from DHLLDV import DHLLDV_framework
 from DHLLDV.SlurryObj import Slurry
 
 pipe = collections.namedtuple('pipe',
-                              ('name', 'object', 'diameter', 'length', 'total_K', 'elev_change'),
+                              ('name', 'diameter', 'length', 'total_K', 'elev_change'),
                               )
 
-pipeline = (pipe('Entrance', None, 0, 0, 0.5, -10.0),
-            pipe('Suction', Slurry(Dp=0.863), 0.863, 38.0, 0.35, 10),
-            pipe('Discharge', Slurry(), 0.762, 1000, 1.0, 1.5))
+class Pipeline():
+    """Object to manage the pipeline system"""
+    def __init__(self, pipe_list=None, slurry=None):
+        if not slurry:
+            self.slurry = Slurry()
+        else:
+            self.slurry = slurry
+        if pipe_list:
+            self.pipesections = pipe_list
+        else:
+            self.pipesections = [pipe('Entrance', self.slurry.Dp, 0, 0.5, -10.0),
+                                 pipe('Discharge', self.slurry.Dp, 1000, 1.0, 1.5)]
+        self.slurries = {self.slurry.Dp: self.slurry}
+        for p in self.pipesections:
+            if p.diameter not in self.slurries:
+                self.slurries[p.diameter] = copy(self.slurry)
+                self.slurries[p.diameter].Dp = p.diameter
+
+pipeline = Pipeline()
+
+def calc_system_head(PL, v, Cv=None):
+    """Calculate the system head for a pipeline"""
+    fitting_loss = 0
+    elev_head = 0
+
+    for p in pipe:
+        if Cv and type(p.object) == Slurry:
+            p.object.Cv = Cv
 
 def update_all():
     """Placeholder for an update function"""
@@ -30,7 +56,7 @@ def update_all():
 
 def pipe_panel(pipe):
     """Create a Bokeh row with information about the pipe"""
-    return row(TextInput(title="Name", value=pipe.name, width=76),
+    return row(TextInput(title="Name", value=pipe.name, width=96),
                TextInput(title="Dp (mm)", value=f"{int(pipe.diameter*1000)}", width=76),
                TextInput(title="Length (m)", value=f"{pipe.length:0.1f}", width=76),
                TextInput(title="Fitting K (-)", value=f"{pipe.total_K:0.2f}", width=76),
@@ -38,7 +64,7 @@ def pipe_panel(pipe):
 
 def system_panel(PL):
     """Create a Bokeh Panel with the system elements"""
-    return Panel(title="Pipeline", child = column([pipe_panel(p) for p in PL]))
+    return Panel(title="Pipeline", child = column([pipe_panel(p) for p in PL.pipesections]))
 
 
 
