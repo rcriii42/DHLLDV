13ead15c6a76526a0a7664722657e3c072b0baba
rcriii42 <rcriii42@gmail.com>
2021-09-22 13:44:22 -0500

    Move all of the Pipeline tab generation into the SystemTab function

--- Scripts/SystemTab.py
+++ Scripts/SystemTab.py
@@ -32,135 +32,136 @@ pipeline.pipesections = [Pipe('Entrance',      0.864,  0.0, 0.50, -10.0),
                          Pipe('Shore Pipe',    0.762,750.0, 9.80,   0.0)]
 pipeline.update_slurries()
 
-flow_list = [pipeline.pipesections[-1].flow(v) for v in pipeline.slurry.vls_list]
-head_lists = list(zip(*[pipeline.calc_system_head(Q) for Q in flow_list]))
-im_source = ColumnDataSource(data=dict(Q=flow_list,
-                                       v=pipeline.slurry.vls_list,
-                                       im=head_lists[0],
-                                       il=head_lists[1],
-                                       Hpump_l = head_lists[2],
-                                       Hpump_m = head_lists[3]))
+def system_panel(PL):
+    """Create a Bokeh Panel with the pipelinen and an overall HQ plot"""
 
-HQ_TOOLTIPS = [('name', "$name"),
-               ("Flow (m\u00b3/sec)", "@Q{0.00}"),
-               ("Velocity (m/sec)", "@v{0.00}"),
-               ("Slurry Graded Cvt=c (m/m)", "@im{0,0.0}"),
-               ("Fluid (m/m)", "@il{0,0.0}"),
-               ("Pump Head Slurry (m)", "@Hpump_m{0,0.0}",),
-               ("Pump Head Water (m)", "@Hpump_l{0,0.0}",),
-              ]
-HQ_plot = figure(height=450, width=725, title="System Head Requirement",
-                 tools="crosshair,pan,reset,save,wheel_zoom",
-                 #x_range=[0, 10], y_range=[0, 0.6],
-                 tooltips=HQ_TOOLTIPS)
-
-HQ_plot.line('Q', 'im', source=im_source,
-             color='black',
-             line_dash='dashed',
-             line_width=3,
-             line_alpha=0.6,
-             legend_label='Slurry Graded Cvt=c (m/m)',
-             name='Slurry graded Sand Cvt=c')
-
-HQ_plot.line('Q', 'il', source=im_source,
-             color='blue',
-             line_dash='dashed',
-             line_width=2,
-             line_alpha=0.3,
-             legend_label='Water',
-             name='Water')
-
-HQ_plot.line('Q', 'Hpump_m', source=im_source,
-             color='black',
-             line_dash='dashed',
-             line_width=3,
-             line_alpha=0.6,
-             legend_label='Pump Slurry',
-             name='Pump Slurry')
-
-HQ_plot.line('Q', 'Hpump_l', source=im_source,
-             color='blue',
-             line_dash='dashed',
-             line_width=2,
-             line_alpha=0.3,
-             legend_label='Pump Water',
-             name='Pump Water')
-HQ_plot.extra_x_ranges = {'vel_range': Range1d(pipeline.slurry.vls_list[0], pipeline.slurry.vls_list[-1])}
-HQ_plot.add_layout(LinearAxis(x_range_name='vel_range'), 'above')
-HQ_plot.xaxis[1].axis_label = f'Flow (m\u00b3/sec)'
-HQ_plot.xaxis[0].axis_label = f'Velocity (m/sec in {pipeline.slurry.Dp:0.3f}m pipe)'
-HQ_plot.yaxis[0].axis_label = 'Head (m/m)'
-HQ_plot.y_range.end = 2 * pipeline.calc_system_head(0.1)[3]
-HQ_plot.axis.major_tick_in = 10
-HQ_plot.axis.minor_tick_in = 7
-HQ_plot.axis.minor_tick_out = 0
-
-
-HQ_plot.legend.location = "top_left"
-
-def update_all(pipeline):
-    """Update the data sources and information boxes"""
     flow_list = [pipeline.pipesections[-1].flow(v) for v in pipeline.slurry.vls_list]
     head_lists = list(zip(*[pipeline.calc_system_head(Q) for Q in flow_list]))
-    im_source.data=dict(Q=flow_list,
-                        v=pipeline.slurry.vls_list,
-                        im=head_lists[0],
-                        il=head_lists[1],
-                        Hpump_l = head_lists[2],
-                        Hpump_m = head_lists[3])
+    im_source = ColumnDataSource(data=dict(Q=flow_list,
+                                           v=pipeline.slurry.vls_list,
+                                           im=head_lists[0],
+                                           il=head_lists[1],
+                                           Hpump_l = head_lists[2],
+                                           Hpump_m = head_lists[3]))
+
+    HQ_TOOLTIPS = [('name', "$name"),
+                   ("Flow (m\u00b3/sec)", "@Q{0.00}"),
+                   ("Velocity (m/sec)", "@v{0.00}"),
+                   ("Slurry Graded Cvt=c (m/m)", "@im{0,0.0}"),
+                   ("Fluid (m/m)", "@il{0,0.0}"),
+                   ("Pump Head Slurry (m)", "@Hpump_m{0,0.0}",),
+                   ("Pump Head Water (m)", "@Hpump_l{0,0.0}",),
+                  ]
+    HQ_plot = figure(height=450, width=725, title="System Head Requirement",
+                     tools="crosshair,pan,reset,save,wheel_zoom",
+                     #x_range=[0, 10], y_range=[0, 0.6],
+                     tooltips=HQ_TOOLTIPS)
+
+    HQ_plot.line('Q', 'im', source=im_source,
+                 color='black',
+                 line_dash='dashed',
+                 line_width=3,
+                 line_alpha=0.6,
+                 legend_label='Slurry Graded Cvt=c (m/m)',
+                 name='Slurry graded Sand Cvt=c')
+
+    HQ_plot.line('Q', 'il', source=im_source,
+                 color='blue',
+                 line_dash='dashed',
+                 line_width=2,
+                 line_alpha=0.3,
+                 legend_label='Water',
+                 name='Water')
+
+    HQ_plot.line('Q', 'Hpump_m', source=im_source,
+                 color='black',
+                 line_dash='dashed',
+                 line_width=3,
+                 line_alpha=0.6,
+                 legend_label='Pump Slurry',
+                 name='Pump Slurry')
+
+    HQ_plot.line('Q', 'Hpump_l', source=im_source,
+                 color='blue',
+                 line_dash='dashed',
+                 line_width=2,
+                 line_alpha=0.3,
+                 legend_label='Pump Water',
+                 name='Pump Water')
+    HQ_plot.extra_x_ranges = {'vel_range': Range1d(pipeline.slurry.vls_list[0], pipeline.slurry.vls_list[-1])}
+    HQ_plot.add_layout(LinearAxis(x_range_name='vel_range'), 'above')
+    HQ_plot.xaxis[1].axis_label = f'Flow (m\u00b3/sec)'
     HQ_plot.xaxis[0].axis_label = f'Velocity (m/sec in {pipeline.slurry.Dp:0.3f}m pipe)'
-    old_disch_dia = pipeline.pipesections[-1].diameter
-    for p, pipe_row in zip(pipeline.pipesections, pipecol.children[1:]):
-        if isinstance(p, Pipe) and p.diameter == old_disch_dia:
-            pipe_row.children[2].value=f"{p.diameter:0.3f}"
-    totalscol.children[0].children[2].value=f"{pipeline.pipesections[-1].diameter:0.3f}"
+    HQ_plot.yaxis[0].axis_label = 'Head (m/m)'
+    HQ_plot.y_range.end = 2 * pipeline.calc_system_head(0.1)[3]
+    HQ_plot.axis.major_tick_in = 10
+    HQ_plot.axis.minor_tick_in = 7
+    HQ_plot.axis.minor_tick_out = 0
 
-def pipe_panel(i, pipe):
-    """Create a Bokeh row with information about the pipe"""
-    if isinstance(pipe, Pipe):
-        return row(TextInput( value=f'{i:3d}', width=45),
-                   TextInput(value=pipe.name, width=95),
-                   TextInput(value=f"{pipe.diameter:0.3f}", width=76),
-                   TextInput( value=f"{pipe.length:0.1f}", width=76),
-                   TextInput( value=f"{pipe.total_K:0.2f}", width=76),
-                   TextInput(value=f"{pipe.elev_change:0.1f}", width=76),)
-    elif isinstance(pipe, Pump):
-        return row(TextInput(title="#", value=f'{i:3d}', width=45, background='lightblue'),
-                   TextInput(title="Pump", value=pipe.name, width=95, background='lightblue'),
-                   TextInput(title="Suction (m)", value=f"{pipe.suction_dia:0.3f}", width=76, background='lightblue'),
-                   TextInput(title="Discharge (m)", value=f"{pipe.disch_dia:0.3f}", width=76, background='lightblue'),
-                   TextInput(title="Impeller (m)", value=f"{pipe.design_impeller:0.3f}", width=76, background='lightblue'),
-                   TextInput(title="Power (kW)", value=f"{pipe.avail_power:0.0f}", width=76, background='lightblue'),)
-    else:
-        return row(TextInput(value="Unknown Item", width=76),)
 
-def pipeline_totals():
-    """Create rows for totals in the pipeline"""
-    pipe_totals = row(TextInput(title="#", value=f'{pipeline.num_pipesections:3d}', width=45, disabled=True),
-                      TextInput(title="Pipe Sections", value="Total", width=95, disabled=True),
-                      TextInput(title="Disch Dia (m)", value=f"{pipeline.pipesections[-1].diameter:0.3f}", width=76, disabled=True),
-                      TextInput(title="Length (m)", value=f"{sum([pipeline.total_length]):0.1f}", width=76, disabled=True),
-                      TextInput(title="Fitting K (-)", value=f"{pipeline.total_K:0.2f}", width=76, disabled=True),
-                      TextInput(title="Delta z (m)", value=f"{pipeline.total_lift:0.1f}", width=76, disabled=True),)
-    shutoff_head = pipeline.calc_system_head(0.01)
-    pump_totals = row(TextInput(title="#", value=f'{pipeline.num_pumps:3d}', width=45, disabled=True),
-                      TextInput(title="Pumps", value="Total", width=95, disabled=True),
-                      TextInput(title="Head Water", value=f"{shutoff_head[2]:0.0f}", width=95, disabled=True),
-                      TextInput(title="Head Slurry", value=f"{shutoff_head[3]:0.0f}", width=95, disabled=True),
-                      TextInput(title="Power (kW)", value=f"{pipeline.total_power:0.0f}", width=95, disabled=True),)
-    return column(pipe_totals, pump_totals)
-totalscol = pipeline_totals()
+    HQ_plot.legend.location = "top_left"
 
-pipecol = column(row(TextInput( value=f'#', width=45, disabled=True),
-                     TextInput(value='name', width=95, disabled=True),
-                     TextInput(value=f"diameter (m)", width=76, disabled=True),
-                     TextInput( value=f"Length (m)", width=76, disabled=True),
-                     TextInput( value=f"Total K", width=76, disabled=True),
-                     TextInput(value=f"Delta z (m)", width=76, disabled=True),))
-[pipecol.children.append(pipe_panel(i, p)) for i, p in enumerate(pipeline.pipesections)]
+    def update_all(pipeline):
+        """Update the data sources and information boxes"""
+        flow_list = [pipeline.pipesections[-1].flow(v) for v in pipeline.slurry.vls_list]
+        head_lists = list(zip(*[pipeline.calc_system_head(Q) for Q in flow_list]))
+        im_source.data=dict(Q=flow_list,
+                            v=pipeline.slurry.vls_list,
+                            im=head_lists[0],
+                            il=head_lists[1],
+                            Hpump_l = head_lists[2],
+                            Hpump_m = head_lists[3])
+        HQ_plot.xaxis[0].axis_label = f'Velocity (m/sec in {pipeline.slurry.Dp:0.3f}m pipe)'
+        old_disch_dia = pipeline.pipesections[-1].diameter
+        for p, pipe_row in zip(pipeline.pipesections, pipecol.children[1:]):
+            if isinstance(p, Pipe) and p.diameter == old_disch_dia:
+                pipe_row.children[2].value=f"{p.diameter:0.3f}"
+        totalscol.children[0].children[2].value=f"{pipeline.pipesections[-1].diameter:0.3f}"
 
-def system_panel(PL):
-    """Create a Bokeh Panel with the system elements"""
+    def pipe_panel(i, pipe):
+        """Create a Bokeh row with information about the pipe"""
+        if isinstance(pipe, Pipe):
+            return row(TextInput( value=f'{i:3d}', width=45),
+                       TextInput(value=pipe.name, width=95),
+                       TextInput(value=f"{pipe.diameter:0.3f}", width=76),
+                       TextInput( value=f"{pipe.length:0.1f}", width=76),
+                       TextInput( value=f"{pipe.total_K:0.2f}", width=76),
+                       TextInput(value=f"{pipe.elev_change:0.1f}", width=76),)
+        elif isinstance(pipe, Pump):
+            return row(TextInput(title="#", value=f'{i:3d}', width=45, background='lightblue'),
+                       TextInput(title="Pump", value=pipe.name, width=95, background='lightblue'),
+                       TextInput(title="Suction (m)", value=f"{pipe.suction_dia:0.3f}", width=76, background='lightblue'),
+                       TextInput(title="Discharge (m)", value=f"{pipe.disch_dia:0.3f}", width=76, background='lightblue'),
+                       TextInput(title="Impeller (m)", value=f"{pipe.design_impeller:0.3f}", width=76, background='lightblue'),
+                       TextInput(title="Power (kW)", value=f"{pipe.avail_power:0.0f}", width=76, background='lightblue'),)
+        else:
+            return row(TextInput(value="Unknown Item", width=76),)
+
+    def pipeline_totals():
+        """Create rows for totals in the pipeline"""
+        pipe_totals = row(TextInput(title="#", value=f'{pipeline.num_pipesections:3d}', width=45, disabled=True),
+                          TextInput(title="Pipe Sections", value="Total", width=95, disabled=True),
+                          TextInput(title="Disch Dia (m)", value=f"{pipeline.pipesections[-1].diameter:0.3f}", width=76, disabled=True),
+                          TextInput(title="Length (m)", value=f"{sum([pipeline.total_length]):0.1f}", width=76, disabled=True),
+                          TextInput(title="Fitting K (-)", value=f"{pipeline.total_K:0.2f}", width=76, disabled=True),
+                          TextInput(title="Delta z (m)", value=f"{pipeline.total_lift:0.1f}", width=76, disabled=True),)
+        shutoff_head = pipeline.calc_system_head(0.01)
+        pump_totals = row(TextInput(title="#", value=f'{pipeline.num_pumps:3d}', width=45, disabled=True),
+                          TextInput(title="Pumps", value="Total", width=95, disabled=True),
+                          TextInput(title="Head Water", value=f"{shutoff_head[2]:0.0f}", width=95, disabled=True),
+                          TextInput(title="Head Slurry", value=f"{shutoff_head[3]:0.0f}", width=95, disabled=True),
+                          TextInput(title="Power (kW)", value=f"{pipeline.total_power:0.0f}", width=95, disabled=True),)
+        return column(pipe_totals, pump_totals)
+    totalscol = pipeline_totals()
+
+    pipecol = column(row(TextInput( value=f'#', width=45, disabled=True),
+                         TextInput(value='name', width=95, disabled=True),
+                         TextInput(value=f"diameter (m)", width=76, disabled=True),
+                         TextInput( value=f"Length (m)", width=76, disabled=True),
+                         TextInput( value=f"Total K", width=76, disabled=True),
+                         TextInput(value=f"Delta z (m)", width=76, disabled=True),))
+    [pipecol.children.append(pipe_panel(i, p)) for i, p in enumerate(pipeline.pipesections)]
+
     return Panel(title="Pipeline", child = row(column(totalscol,
                                                       Spacer(background='lightblue', height=5, margin=(5, 0, 5, 0)),
                                                       pipecol),
