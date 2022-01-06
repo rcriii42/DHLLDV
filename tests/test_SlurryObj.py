0b4aac392194dfbf4f5eb58c5e5b5375422cf5d4
rcriii42 <rcriii42@gmail.com>
2022-01-06 10:01:18 -0600

    Tests of il, Erhg, im

--- tests/test_SlurryObj.py
+++ tests/test_SlurryObj.py
@@ -9,7 +9,16 @@ class Test(unittest.TestCase):
 
     def test_il(self):
         vls = self.slurry.vls_list[42]
-        self.assertEqual(self.slurry.il(vls), self.slurry.Erhg_curves[42]['il'])
+        self.assertEqual(self.slurry.il(vls), self.slurry.Erhg_curves['il'][42])
+        self.assertEqual(self.slurry.il(vls), self.slurry.im_curves['il'][42])
+
+    def test_Erhg(self):
+        vls = self.slurry.vls_list[42]
+        self.assertEqual(self.slurry.Erhg(vls), self.slurry.Erhg_curves['graded_Cvt_Erhg'][42])
+
+    def test_im(self):
+        vls = self.slurry.vls_list[42]
+        self.assertEqual(self.slurry.im(vls), self.slurry.im_curves['graded_Cvt_im'][42])
 
 
 if __name__ == '__main__':
