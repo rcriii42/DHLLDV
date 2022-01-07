efbaeb1a29cea3af9fbf311c0f345315b917309d
rcriii42 <rcriii42@gmail.com>
2022-01-07 13:36:48 -0600

    Add tests of small grain sizes
    Roll all the get_dx tests into two functions rather than individual per dx

--- tests/test_SlurryObj.py
+++ tests/test_SlurryObj.py
@@ -9,28 +9,29 @@ class Test(unittest.TestCase):
         self.slurry.Dp = 0.5
         self.slurry.generate_curves()
 
-    def test_dx_10(self):
-        """Test the calculation of d10"""
+    def test_dx_1mm(self):
+        """Test the calculation of GSD for 1mm D50"""
         self.assertAlmostEqual(self.slurry.get_dx(0.10)*1000, 0.4528618)
-
-    def test_dx_15(self):
-        """Test the calculation of d15"""
         self.assertAlmostEqual(self.slurry.get_dx(0.15)*1000, 0.50)
-
-    def test_dx_42(self):
-        """Test the calculation of d42"""
         self.assertAlmostEqual(self.slurry.get_dx(0.42)*1000, 0.8534796)
-    def test_dx50(self):
-        """Test the calculation of d50"""
-        self.assertAlmostEqual(self.slurry.get_dx(0.50) * 1000, 1.0)
-
-    def test_dx_69(self):
-        """Test the calculation of d69"""
+        self.assertAlmostEqual(self.slurry.get_dx(0.50)*1000, 1.0)
         self.assertAlmostEqual(self.slurry.get_dx(0.69)*1000, 1.7215072)
+        self.assertAlmostEqual(self.slurry.get_dx(0.85)*1000, 2.72)
 
-    def test_dx85(self):
-        """Test the calculation of d85"""
-        self.assertAlmostEqual(self.slurry.get_dx(0.85) * 1000, 2.72)
+    def test_dx_0p22mm(self):
+        """Test the calculation of GSD for 0.22mm D50
+
+        This tests the case where the Xmin > 0.1"""
+        s = SlurryObj.Slurry()
+        s.D50 = 0.22/1000
+        s.generate_GSD()
+        self.assertAlmostEqual(min(s.GSD.keys()), 0.156748040)
+        self.assertAlmostEqual(s.get_dx(0.10)*1000, 0.0996296)
+        self.assertAlmostEqual(s.get_dx(0.15)*1000, 0.11)
+        self.assertAlmostEqual(s.get_dx(0.42)*1000, 0.1877655)
+        self.assertAlmostEqual(s.get_dx(0.50)*1000, 0.22)
+        self.assertAlmostEqual(s.get_dx(0.69)*1000, 0.3787316)
+        self.assertAlmostEqual(s.get_dx(0.85)*1000, 0.598400)
 
     def test_il_fresh(self):
         vls = self.slurry.vls_list[42]
