81b188c8010df606daee32d21e8b4af5281244c5
rcriii42 <rcriii42@gmail.com>
2022-09-13 11:55:37 -0500

    Remove spacer and widths from top row widgets

--- Scripts/bokeh_viewer.py
+++ Scripts/bokeh_viewer.py
@@ -508,7 +508,8 @@ sys_tab, sys_update = SystemTab.system_panel(pipeline)
 
 tabbed_panels = Tabs(tabs=[slurry_panel, sys_tab])
 tabbed_panels.active = 1
-curdoc().add_root(column(row(Spacer(width=500), pipeline_dropdown, unit_picker, stop_button),
+top_row = row(pipeline_dropdown, unit_picker, stop_button)
+curdoc().add_root(column(top_row,
                          tabbed_panels))
 
 curdoc().title = "Visualizing DHLLDV"
