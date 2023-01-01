e71f707bd204058caa862902f0fee9088a4f2284
rcriii42 <rcriii42@gmail.com>
2023-01-01 17:48:16 -0600

    Load driver if present

--- DHLLDV_viewer/load_pump_excel.py
+++ DHLLDV_viewer/load_pump_excel.py
@@ -7,7 +7,7 @@ The spreadsheet should have sheets with the following properties:
 - Sheets without 'pipeline', 'pump, 'driver' in the name are ignored
 - One worksheet named 'Pipeline'
     - Have a named range 'Pipeline!name
-- One or more worksheets with the word 'pump' in the name
+- One or more worksheets whose name ends in the word 'pump'
     - Have named ranges SomePump!['name', 'design_impeller', 'suction_dia', 'disch_dia','design_speed', 'limited',
     'gear_ratio', 'avail_power','pump_curve']
     - name is any valid python string (be careful of escape sequences)
@@ -33,6 +33,7 @@ The spreadsheet should have sheets with the following properties:
 import openpyxl
 
 from DHLLDV.PumpObj import Pump
+from DHLLDV.DriverObj import Driver
 from DHLLDV.DHLLDV_Utils import interpDict
 
 
@@ -43,10 +44,34 @@ def get_range_value(wb: openpyxl.Workbook, sheet_id: int, range_name: str):
     return wb[sheet_name][sheet_addr].value
 
 
-def load_pump_from_worksheet(wb: openpyxl.Workbook, sheet_id: int):
+def load_driver_from_worksheet(wb: openpyxl.Workbook, sheet_id: int):
+    """Create a driver object from a driver worksheet
+    wb: The workbook to load
+    sheet_id: The id of the driver sheet in the sheet list
+    """
+    params = {'name': get_range_value(wb, sheet_id, 'name')}
+    my_range = wb.defined_names.get('power_curve', sheet_id)
+    speed_col = None
+    power_col = None
+    cell_range = next(my_range.destinations)  # returns a generator of (worksheet title, cell range) tuples
+    rows = wb[wb.sheetnames[sheet_id]][cell_range[1]]
+    curve = {}
+    for row in rows:
+        vals = [c.value for c in row]
+        if speed_col is None:  # Assume the first row is a header
+            speed_col = next(i for i, c in enumerate(vals) if 'speed' in c.lower())
+            power_col = next(i for i, c in enumerate(vals) if 'power' in c.lower())
+        else:
+            curve[float(vals[speed_col])] = float(vals[power_col])
+    params['design_power_curve'] = curve
+    return Driver(**params)
+
+
+def load_pump_from_worksheet(wb: openpyxl.Workbook, sheet_id: int, driver_id: int = None):
     """Create a pump object from a pump worksheet
     wb: The workbook to load
     sheet_id: The id of the pump sheet in the sheet list
+    driver_id: The id of any driver in the sheet list, or None if no driver
     """
     single_values = {'name': str,
                      'design_impeller': float,
@@ -74,12 +99,14 @@ def load_pump_from_worksheet(wb: openpyxl.Workbook, sheet_id: int):
             flow_col = next(i for i, c in enumerate(vals) if 'flow' in c.lower())
             head_col = next(i for i, c in enumerate(vals) if 'head' in c.lower())
             power_col = next(i for i, c in enumerate(vals) if 'power' in c.lower())
-            print(flow_col, head_col, power_col)
         else:
             QH[float(vals[flow_col])] = float(vals[head_col])
             QP[float(vals[flow_col])] = float(vals[power_col])
     params['design_QH_curve'] = interpDict(QH)
     params['design_QP_curve'] = interpDict(QP)
+    if driver_id is not None:
+        params['driver'] = load_driver_from_worksheet(wb, driver_id)
+        params['driver_name'] = params['driver'].name
     return Pump(**params)
 
 
@@ -87,6 +114,8 @@ if __name__ == "__main__":
     fname = "static/pipelines/Example_input.xlsx"
 
     wb = openpyxl.load_workbook(filename=fname, data_only=True) # Loading a workbook, data_only takes the stored values
+    pumps = {}
+    drivers = {}
     for ws_name in wb.sheetnames:
         sheet_id = wb.sheetnames.index(ws_name)       # The id / index of a worksheet
         ret_val = ''
@@ -96,11 +125,18 @@ if __name__ == "__main__":
             print(f'Pipeline: {pipeline_name}')
         elif 'pump' in ws_name.lower():
             input_type = 'pump'
-            ret_val = load_pump_from_worksheet(wb, sheet_id)
+            pumps[ws_name.lower().removesuffix('pump')] = sheet_id
         elif 'driver' in ws_name.lower():
             input_type = 'driver'
+            drivers[ws_name.lower().removesuffix('driver')] = sheet_id
         else:
             input_type = ws_name
         print(f'{sheet_id} {input_type}: {wb[ws_name]}')        # Accessing individual worksheets
         print(wb.defined_names.localnames(sheet_id))  # Range names in a worksheet
         print(ret_val)
+    for pump_name in pumps:
+        print(pump_name)
+        if pump_name in drivers:
+            print(load_pump_from_worksheet(wb, pumps[pump_name], drivers[pump_name]))
+        else:
+            print(load_pump_from_worksheet(wb, pumps[pump_name], None))
