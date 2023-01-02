e35362d57d1e186e55f3af95f909f1817ff84373
rcriii42 <rcriii42@gmail.com>
2023-01-01 23:41:18 -0600

    Allow a tolerance on the input key to allow for small rounding errors
    Document parameters

--- src/DHLLDV/DHLLDV_Utils.py
+++ src/DHLLDV/DHLLDV_Utils.py
@@ -10,6 +10,11 @@ import bisect
 class interpDict(dict):
     """
     interpDict: Dict of two-tuples that will interpolate values not found in the dict.
+
+    The key/value pairs are floats x, f(x), assumes f(x) is continuous on the bounds of x, sorted on x low to high
+    extrapolate_low: True if the interpolation should be continued below the low end of the range (default=False)
+    extrapolate_high: True if the interpolation should be continued above the high end of the range (default=False)
+    tolerance: By how much x is allowed to exceed the top or bottom end
     """
     def __init__(self, *args, **kwargs):
         #print type(args[0]), args
@@ -19,6 +24,7 @@ class interpDict(dict):
             dict.__init__(self, args)
         self.extrapolate_low = kwargs.setdefault('extrapolate_low', False)
         self.extrapolate_high = kwargs.setdefault('extrapolate_high', False)
+        self.tolerance = kwargs.setdefault('tolerance', 0.001)
 
     def __getitem__(self, key):
         try:
@@ -32,13 +38,13 @@ class interpDict(dict):
                 y1 = dict.__getitem__(self, x1)
                 y2 = dict.__getitem__(self, x2)
                 val = ((y2-y1)/(x2-x1))*(key-x1)+y1
-            elif index == len(keys) and self.extrapolate_high:
+            elif index == len(keys) and (self.extrapolate_high or key <= max(keys)*(1+self.tolerance)):
                 x1 = keys[-2]
                 x2 = keys[-1]
                 y1 = dict.__getitem__(self, x1)
                 y2 = dict.__getitem__(self, x2)
                 val = ((y2 - y1) / (x2 - x1)) * (key - x1) + y1
-            elif index == 0 and self.extrapolate_low:
+            elif index == 0 and (self.extrapolate_low or key >= min(keys)*(1-self.tolerance)):
                 x1 = keys[0]
                 x2 = keys[1]
                 y1 = dict.__getitem__(self, x1)
