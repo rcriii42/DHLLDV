7579ea7afaeb2f826ce4f028f29b94e20821cea9
Robert Ramsdell <Robert.Ramsdell@DredgingResources.net>
2025-05-09 15:30:13 -0400

    Create Slug object to manage slugs (len, Slurry) of material, and tests
    Tests pass

--- src/DHLLDV/LagrPipe.py
+++ src/DHLLDV/LagrPipe.py
@@ -3,11 +3,10 @@
 Creates a Lagrangian view of the pipeline that tracks 'slugs' of slurry moving down the pipeline
 """
 
-from collections.abc import Callable, Awaitable
+from collections.abc import Callable
 from copy import copy
 from dataclasses import dataclass
 from math import pi
-from types import FunctionType
 
 import scipy.optimize
 from DHLLDV.DHLLDV_constants import gravity
@@ -30,52 +29,57 @@ def add_slurries(sw1: [Slurry | None, float], sw2: [Slurry | None, float]) -> Sl
     return new_slurry
 
 
+@dataclass
+class Slug:
+    """A slug of slurry in the pipeline"""
+    length: float
+    slurry: Slurry
+
 
 @dataclass
 class LagrPipe(Pipe):
     """A Lagrangian view of the pipe that tracks 'slugs' of slurry moving through the pipe section """
-    slugs: list[(float, Slurry)] | None = None  # Length and Slurry of each slug
-    feed_in: Callable[[float], tuple[float, Slurry]] | None = None  # Stub for the feed function
+    slugs: list[Slug] | None = None  # Length and Slurry of each slug
+    feed_in: Callable[[float], (float, Slug)] | None = None  # The incoming feed function, see definition below
 
     def __post_init__(self):
-        self.length = sum(s[0] for s in self.slugs)
+        self.length = sum(s.length for s in self.slugs)
 
-    def feed(self, Q: float) -> (float, Slurry):
+    def feed(self, Q: float) -> (float, Slug):
         """
         Get slurry from upstream, update the slugs, and send slurry and head downstream
 
         Q is the quantity moved this timestep in m3
 
-        Returns a tuple of the total head requirement to date, and the Slurry passed downstream
+        Returns a tuple of the total head requirement to date, and the Slug passed downstream
         """
-        in_h, in_slurry = self.feed_in(Q)
-        in_slurry.Dp = self.slugs[0][1].Dp
+        h_in, in_slug = self.feed_in(Q)
+        in_slug.slurry.Dp = self.slugs[0].slurry.Dp
         slug_length = self.velocity(Q)  # If the timestep is 1, the velocity is the slug length
 
-        self.slugs.insert(0, (slug_length, in_slurry))
+        self.slugs.insert(0, in_slug)
 
         extruded_len = 0
         extruded_slurry = None
         while extruded_len < slug_length:
             last_slug = self.slugs[-1]
             remain_len = slug_length - extruded_len
-            if last_slug[0] <= remain_len:
+            if last_slug.length <= remain_len:
                 # Add the entire slug to the extruded slug
                 extruded_slurry = add_slurries((extruded_slurry, extruded_len),
-                                               (last_slug[1], last_slug[0]))
-                extruded_len += last_slug[0]
+                                               (last_slug.slurry, last_slug.length))
+                extruded_len += last_slug.length
                 self.slugs = self.slugs[:-1]
             else:
                 # The last slug is too long, split it
                 extruded_slurry = add_slurries((extruded_slurry, extruded_len),
-                                               (last_slug[1], remain_len))
+                                               (last_slug.slurry, remain_len))
                 extruded_len += remain_len
-                self.slugs[-1] = (last_slug[0] - remain_len, last_slug[1])
+                self.slugs[-1].length = last_slug.length - remain_len
 
         for s in self.slugs:
-            im = s[1].im(Q)  # Implicitly assume timestep is 1
+            im = s.slurry.im(slug_length)  # Implicitly assume timestep is 1
             hvel = extruded_len**2 / (2 * gravity)
-            in_h += im * s[0] + s[1].rhom * (self.total_K * s[0]/self.length) * hvel
+            h_in += im * s.length + s.slurry.rhom * (self.total_K * s.length/self.length) * hvel
 
-        return in_h, extruded_slurry
-
+        return h_in, Slug(extruded_len, extruded_slurry)
--- tests/test_lagrPipe.py
+++ tests/test_lagrPipe.py
@@ -1,6 +1,6 @@
 import unittest
 
-from DHLLDV.LagrPipe import add_slurries, LagrPipe
+from DHLLDV.LagrPipe import add_slurries, LagrPipe, Slug
 from DHLLDV.SlurryObj import Slurry
 
 
@@ -8,7 +8,7 @@ class MyTestCase(unittest.TestCase):
     def setUp(self):
         self.slurry1 = Slurry(Cv=0.2)
         self.slurry2 = Slurry(Cv=0.1)
-        self.l_pipe = LagrPipe(slugs=[(10.0, self.slurry1)])
+        self.l_pipe = LagrPipe(slugs=[Slug(10.0, self.slurry1)])
 
     def test_add_slurries_first_none(self):
         new_slurry = add_slurries((None, 1), (self.slurry2, 1))
@@ -29,48 +29,50 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(new_slurry.rhom, rho_expect)
 
     def test_init_lagrpipe(self):
-        total_length = sum(s[0] for s in self.l_pipe.slugs)
-        self.assertEqual(10, total_length)
+        total_length = sum(s.length for s in self.l_pipe.slugs)
+        self.assertAlmostEqual(10, total_length)
 
     def test_feed_rhom_out(self):
-        self.l_pipe.feed_in = lambda q: (1.0, self.slurry2)
-        h_out, slurry_out = self.l_pipe.feed(1.824146925)
-        self.assertEqual(slurry_out.rhom, 1.34984824)
+        self.l_pipe.feed_in = lambda q: (1.0, Slug(4.0, self.slurry2))
+        h_out, slug_out = self.l_pipe.feed(1.824146925)
+        self.assertEqual(slug_out.slurry.rhom, 1.34984824)
+        self.assertAlmostEqual(slug_out.length, 4)
 
-    def test_feed5_step_1(self):
-        """Test the first step if velocity is 5 m/sec"""
-        self.l_pipe.feed_in = lambda q: (1.0, self.slurry2)
+    def test_feed4_step_1(self):
+        """Test the first step if velocity is 4 m/sec"""
+        self.l_pipe.feed_in = lambda q: (1.0, Slug(4.0, self.slurry2))
         h_out, slurry_out = self.l_pipe.feed(1.824146925)
-        total_length = sum(s[0] for s in self.l_pipe.slugs)
-        self.assertEqual(10, total_length)
+        total_length = sum(s.length for s in self.l_pipe.slugs)
+        self.assertAlmostEqual(10, total_length)
         self.assertEqual(len(self.l_pipe.slugs), 2)
-        self.assertEqual(self.l_pipe.slugs[0][1].rhom, self.slurry2.rhom)
-        self.assertEqual(self.l_pipe.slugs[1][1].rhom, self.slurry1.rhom)
-        self.assertEqual(h_out, 1 + 0.744833687)
+        self.assertEqual(self.l_pipe.slugs[0].slurry.rhom, self.slurry2.rhom)
+        self.assertEqual(self.l_pipe.slugs[1].slurry.rhom, self.slurry1.rhom)
+        self.assertAlmostEqual(h_out, 1 + 1.068679635)
 
-    def test_feed5_step_2(self):
-        """Test the second step if velocity is 5 m/sec"""
-        self.l_pipe.feed_in = lambda q: (1.0, self.slurry2)
-        h_out, slurry_out = self.l_pipe.feed(1.824146925)
-        h_out, slurry_out = self.l_pipe.feed(1.824146925)
-        total_length = sum(s[0] for s in self.l_pipe.slugs)
-        self.assertEqual(10, total_length)
+    def test_feed4_step_2(self):
+        """Test the second step if velocity is 4 m/sec"""
+        self.l_pipe.feed_in = lambda q: (1.0, Slug(4.0, self.slurry2))
+        for i in range(2):
+            h_out, slug_out = self.l_pipe.feed(1.824146925)
+        total_length = sum(s.length for s in self.l_pipe.slugs)
+        self.assertAlmostEqual(10, total_length)
         self.assertEqual(len(self.l_pipe.slugs), 3)
-        self.assertEqual(self.l_pipe.slugs[0][1].rhom, self.slurry2.rhom)
-        self.assertEqual(self.l_pipe.slugs[1][1].rhom, self.slurry2.rhom)
-        self.assertEqual(self.l_pipe.slugs[2][1].rhom, self.slurry1.rhom)
+        self.assertEqual(self.l_pipe.slugs[0].slurry.rhom, self.slurry2.rhom)
+        self.assertEqual(self.l_pipe.slugs[1].slurry.rhom, self.slurry2.rhom)
+        self.assertEqual(self.l_pipe.slugs[2].slurry.rhom, self.slurry1.rhom)
+        self.assertAlmostEqual(h_out, 1 + 1.004257308)
 
     def test_feed5p5_step_2(self):
         """Test the second step if velocity is 5.5 m/sec"""
-        self.l_pipe.feed_in = lambda q: (1.0, self.slurry2)
+        self.l_pipe.feed_in = lambda q: (1.0, Slug(5.5, self.slurry2))
         for i in range(2):
-            h_out, slurry_out = self.l_pipe.feed(2.508202022)
-        total_length = sum(s[0] for s in self.l_pipe.slugs)
-        self.assertEqual(10, total_length)
+            h_out, slug_out = self.l_pipe.feed(2.508202022)
+        total_length = sum(s.length for s in self.l_pipe.slugs)
+        self.assertAlmostEqual(10, total_length)
         self.assertEqual(len(self.l_pipe.slugs), 2)
-        self.assertEqual(self.l_pipe.slugs[0][1].rhom, self.slurry2.rhom)
-        self.assertEqual(self.l_pipe.slugs[1][1].rhom, self.slurry2.rhom)
-        self.assertAlmostEqual(slurry_out.rhom, 1.320299336)
+        self.assertEqual(self.l_pipe.slugs[0].slurry.rhom, self.slurry2.rhom)
+        self.assertEqual(self.l_pipe.slugs[1].slurry.rhom, self.slurry2.rhom)
+        self.assertAlmostEqual(slug_out.slurry.rhom, 1.320299336)
 
 
 if __name__ == '__main__':
