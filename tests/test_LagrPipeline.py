c269ad95e59222330cc03737c7eb8d31ad0c0040
Robert Ramsdell <Robert.Ramsdell@DredgingResources.net>
2025-09-17 11:48:31 -0500

    Check head and flow against equivalent pipe object

--- tests/test_LagrPipeline.py
+++ tests/test_LagrPipeline.py
@@ -11,7 +11,14 @@ import unittest
 class MyTestCase(unittest.TestCase):
     def setUp(self) -> None:
         self.pipe = Pipe(diameter=0.6, length=10.0, total_K=0.1, elev_change=5.0)
-        slurry = Slurry(fluid='salt')
+        self.slurry = Slurry(fluid='salt')
+        self.pipe_list = [Pipe('Entrance', 0.6, 0, 0.5, -4.0),
+                          self.pipe,
+                          Ladder_Pump600,
+                          Pipe('MP Suction', 0.5, 25.0, 0.1, 0.0),
+                          Main_Pump500,
+                          Pipe('MP Discharge', diameter=0.5, length=20.0, total_K=0.2, elev_change=-1.0),
+                          Pipe('Discharge', diameter=0.5, length=1000.0, total_K=1.0, elev_change=1.0)]
         self.lpipeline = LagrPipeline(name="test lagrangian pipeline",
                                       pipe_list=[Pipe('Entrance', 0.6, 0, 0.5, -4.0),
                                                  self.pipe,
@@ -20,7 +27,7 @@ class MyTestCase(unittest.TestCase):
                                                  Main_Pump500,
                                                  Pipe('MP Discharge', diameter=0.5, length=20.0, total_K=0.2, elev_change=-1.0),
                                                  Pipe('Discharge', diameter=0.5, length=1000.0, total_K=1.0, elev_change=1.0)],
-                                      slurry=slurry)
+                                      slurry=self.slurry)
 
     def test_lpipeline_length(self):
         """Check the length of the slugs is the length of the pipeline"""
@@ -29,6 +36,24 @@ class MyTestCase(unittest.TestCase):
             if type(p) is LagrPipe:
                 slugs_length += sum(s.length for s in p.slugs)
         self.assertEqual(self.lpipeline.total_length, slugs_length)
+
+    def test_flow_matches_pipeline(self):
+        """Check that the newly initiated lagrpipe flow matched the source pipeline operating point"""
+        P = Pipeline('test_pipeline', self.pipe_list, self.slurry)
+        flow_list = [P.pipesections[-1].flow(v) for v in P.slurry.vls_list]
+        qop = P.find_operating_point(flow_list)
+        # h_losses_slurry, h_losses_fluid, h_pump_slurry, h_pump_fluid = P.calc_system_head(qop)
+        q, h, slug = self.lpipeline.update()
+        self.assertEqual(q, qop)
+
+    def test_head_matches_pipeline(self):
+        """Check that the newly initiated lagrpipe head matches the source pipeline operating point head"""
+        P = Pipeline('test_pipeline', self.pipe_list, self.slurry)
+        flow_list = [P.pipesections[-1].flow(v) for v in P.slurry.vls_list]
+        qop = P.find_operating_point(flow_list)
+        h_losses_slurry, h_losses_fluid, h_pump_fluid, h_pump_slurry = P.calc_system_head(qop)
+        q, h, slug = self.lpipeline.update()
+        self.assertEqual(h, h_losses_slurry - h_pump_slurry)
 
 
 if __name__ == '__main__':
