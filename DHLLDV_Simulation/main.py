a18b2373967f4e39724f1a9108f11ad3b8db383d
Robert Ramsdell <Robert.Ramsdell@DredgingResources.net>
2025-09-25 17:37:28 -0500

    Show average line density

--- DHLLDV_Simulation/main.py
+++ DHLLDV_Simulation/main.py
@@ -36,7 +36,7 @@ lpipeline = LagrPipeline(name="test lagrangian pipeline",
                          slurry=slurry,
                          suct_feed=CyclicFeed(slurry, densities=[1.1, 1.2, 1.3, 1.03], Dp=0.6))
 
-source = ColumnDataSource(data=dict(timestep=[], velocity=[], density=[]))
+source = ColumnDataSource(data=dict(timestep=[], velocity=[], density_in=[], density_avg=[]))
 
 p = figure(height=500, tools="xpan,xwheel_zoom,xbox_zoom,reset",
            y_axis_location="right")
@@ -45,10 +45,12 @@ p.x_range.follow_interval = 100
 p.x_range.range_padding = 0
 
 p.line(x='timestep', y='velocity', alpha=0.2, line_width=3, color='navy', source=source)
-p.line(x='timestep', y='density', alpha=0.8, line_width=2, color='orange', source=source)
+p.line(x='timestep', y='density_in', alpha=0.8, line_width=2, color='orange', source=source)
+p.line(x='timestep', y='density_avg', alpha=0.8, line_width=2, color='red', source=source)
 
 Vm_display = TextInput(title="Vm (m/s)", value=f"{0.0}", width=95, disabled=True)
-Sm_display = TextInput(title="Rhom (ton/m3)", value=f"{0.0}", width=95, disabled=True)
+Sm_in_display = TextInput(title="Rhom in (ton/m3)", value=f"{0.0}", width=100, disabled=True)
+Sm_avg_display = TextInput(title="Rhom avg (ton/m3)", value=f"{0.0}", width=100, disabled=True)
 
 
 def update():
@@ -56,14 +58,16 @@ def update():
     q, heads, disch_slug = lpipeline.update()
     new_data = dict(timestep=[lpipeline.timecounter],
                     velocity=[lpipeline.lpipe_list[-1].velocity(q)],
-                    density=[lpipeline.lpipe_list[0].slugs[0].rhom],)
+                    density_in=[lpipeline.lpipe_list[0].slugs[0].rhom],
+                    density_avg=[lpipeline.average_rhom()],)
 
     Vm_display.value = f'{lpipeline.lpipe_list[-1].velocity(q):0.3f}'
-    Sm_display.value = f'{lpipeline.lpipe_list[0].slugs[0].rhom:0.3f}'
+    Sm_in_display.value = f'{lpipeline.lpipe_list[0].slugs[0].rhom:0.3f}'
+    Sm_avg_display.value = f'{lpipeline.average_rhom():0.3f}'
 
     source.stream(new_data, 100)
 
 
-curdoc().add_root(column(row(Vm_display, Sm_display), p))
+curdoc().add_root(column(row(Vm_display, Sm_in_display, Sm_avg_display), p))
 curdoc().add_periodic_callback(update, 1000)
 curdoc().title = "Simulation"
